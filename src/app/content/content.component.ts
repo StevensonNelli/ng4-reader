c153ff2a4138e381b58317584cfcf1f6fd5db746
StevensonNelli <stevenson.nelli@walkingtree.in>
2017-07-24 10:38:21 +0530

    Changes for md-tab

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,50 +1,61 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ArticleService } from '../article.service';
 import { Article } from '../app.model';
-import {RouterModule, Router} from '@angular/router';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
 @Component({
-  selector: 'app-content',
-  templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+	selector: 'app-content',
+	templateUrl: './content.component.html',
+	styleUrls: ['./content.component.scss']
 })
 export class ContentComponent implements OnInit {
-	
-	@Input() article :any ;
+
+	@Input() article: any;
 	articles: any = [];
-	recentArticle:Article = {};
-	selectedTab:any = 0;
-	
+	recentArticle: Article = {};
+	activatedTab: any = 0;
+
 	constructor(private articleService: ArticleService, private router: Router) { }
 
-	ngOnInit() {
-		//debugger;
-		//this.articles.push(this.article);
-		//this.articles = this.articles.indexOf(this.articles.find(tab => this.router.url.indexOf(tab.link) != -1));
+	ngOnInit() { 
+		let me = this;
+		this.router.events
+			.subscribe((event) => {
+				if (event instanceof NavigationEnd) {
+					let url = event.url;
+					let idIndex = url.lastIndexOf("/")
+					let id = url.substring(idIndex+1);
+					let element;
+					for (let index = 0; index < this.articles.length; index++) {
+						if(id == this.articles[index].id){
+							element = this.articles[index];
+						}
+					}
+					this.articles.indexOf(element);
+					//console.log('NavigationEnd:', st.substring(8+1));
+				}
+			});
 	}
-	getnavLinks(){
+	onTabChange(event) {
+		this.router.navigate(['article', this.recentArticle.id]);
+	}
+	getnavLinks() {
 		//let selectedArticles = this.articleService.getSelectedArticles();
 		this.recentArticle = this.articleService.getRecentArticle();
 		let matched = false;
-		if(this.recentArticle) {
-			if( this.articles.length == 0) {
+		if (this.recentArticle) {
+			if (this.articles.length == 0) {
 				this.articles.push(this.recentArticle);
-				//this.selectedTab = 0;
-
 			} else {
 
-				for(let i = 0; this.articles.length > i; i++){
-					if(this.recentArticle["id"] == this.articles[i].id){
+				for (let i = 0; this.articles.length > i; i++) {
+					if (this.recentArticle["id"] == this.articles[i].id) {
 						matched = true;
-						//this.selectedTab = i
 					}
 				}
-				if(!matched){
+				if (!matched) {
 					this.articles.push(this.recentArticle);
-					//this.selectedTab = this.articles.length;
 				}
 			}
-			
-			//this.selectedTab = this.recentArticle.id || 0;
 		}
 		return this.articles;
 	}
